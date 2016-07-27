6b5ec2ac16609c8ecf1f344192955946af5befb9
gabnie <maxence.bricault@gmail.com>
2016-07-27 14:46:31 +0200

    Install Express generator & socket.io

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.io.emit("socketToMe", "testttteu");
+  res.render('index', { title: 'Zizi' });
 });
 
 module.exports = router;
